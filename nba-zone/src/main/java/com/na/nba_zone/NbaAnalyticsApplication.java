fdecfa909e9da42d474308bb27e6b5aa3a850f0b
Larry Lim <larrylim13542@gmail.com>
2026-01-19 23:11:51 -0800

    initial spring boot setup

---
+++ nba-zone/src/main/java/com/na/nba_zone/NbaAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.na.nba_zone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaAnalyticsApplication.class, args);
+	}
+
+}
---
+++ nba-zone/src/test/java/com/na/nba_zone/NbaAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.na.nba_zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
